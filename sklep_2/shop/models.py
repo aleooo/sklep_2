078e4afdf54feecb3dd6b451ceeb7554e057d981
Aleooo <aleksander.wiedenski@gmail.com>
2021-09-20 13:49:24 +0200

    model column:name,slug,image

--- sklep_2/shop/models.py
+++ sklep_2/shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.query_utils import select_related_descend
 
-# Create your models here.
+
+def get_path_upload_to(instance):
+    return f'products/{instance.name}/{instance.id}'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to=get_path_upload_to)
+    
+
+
+ 
+        
