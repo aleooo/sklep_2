3e7ebfc1ada7a86f9f65f9857ed3f3c09a08fde1
Aleooo <aleksander.wiedenski@gmail.com>
2021-09-22 17:00:04 +0200

    registration panel

--- sklep_2/shop/views.py
+++ sklep_2/shop/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.test import client
 from django.urls import reverse
 
+from .forms import UserModelForm
+
 def main(request):
     return render(request, 'base.html', {'main_bar': True})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserModelForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('shop:login')
+    else:
+        user_form = UserModelForm()
+    return render(request, 'registration/register.html', {'user_form': user_form})
