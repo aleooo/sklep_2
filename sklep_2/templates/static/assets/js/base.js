e8a01d2b2dac7915129d4c47200dbd2a3393db5a
Aleooo <aleksander.wiedenski@gmail.com>
2021-09-28 21:50:27 +0200

    finish basic detail product

--- sklep_2/templates/static/assets/js/base.js
+++ sklep_2/templates/static/assets/js/base.js
@@ -21,17 +21,19 @@ function getCookie(c_name) {
 } 
 function sidebar() {
     if (sidebar_status === true){
+        get_sidebar.style.display = 'block';
         get_sidebar.style.opacity = '1';
-        if($(window).width() < 1600){
+        if($(window).width() < 1600){   
             slid.style.opacity = '0';
         } 
         toggle.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16"><path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/></svg>')
         sidebar_status = false;
     }
     else {
+        get_sidebar.style.display = 'none';
         get_sidebar.style.opacity = '0';
         if($(window).width() < 1600){
-            slid.style.opacity = '1';
+            slid.style.opacity = '1'
         }      
         toggle.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-caret-up" viewBox="0 0 16 16"><path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z"/></svg>')
         sidebar_status = true;
