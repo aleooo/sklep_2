91dabbef1db799631769e44bf38f5157e87e1fa9
Aleooo <aleksander.wiedenski@gmail.com>
2021-10-06 21:03:08 +0200

    toggle cart

--- sklep_2/cart/cart.py
+++ sklep_2/cart/cart.py
@@ -1,4 +1,5 @@
-
+from shop.models import Product
+from decimal import Decimal
 
 class Cart(object):
     def __init__(self, request):
@@ -13,15 +14,25 @@ class Cart(object):
         if id_product in self.cart:
             self.cart[id_product]['quantity'] += quantity
         else:
-            self.cart[id_product] = {'quantity': quantity, 'price': str(product.price), 'image': product.image.url}
+            self.cart[id_product] = {'quantity': quantity, 'price': str(product.price), 'name': product.name, 'image': product.image.url}
         self.session['add'] = True
+        
     
     def error(self, name):
         self.session['error'] = name
-
     
-    
-        
-        
-        
+    def clear(self):
+        del self.session['cart']
 
+
+    def __iter__(self):
+        # id_products = self.cart.keys()
+        # products = Product.objects.filter(id__in=id_products)
+        # cart = self.cart.copy()
+        # for product in products.values():
+        #     self.cart[str(product['id'])]['product'] = product 
+        
+        for item in self.cart.values():
+            item['total_price'] = str(item['quantity'] * Decimal(item['price']))
+            yield item
+        
