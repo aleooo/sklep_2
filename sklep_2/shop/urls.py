a6abf153411a1023974fa2f93a668fd83002c5cd
Aleooo <aleksander.wiedenski@gmail.com>
2021-10-29 20:21:41 +0200

    password change

--- sklep_2/shop/urls.py
+++ sklep_2/shop/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -8,6 +8,7 @@ app_name = 'shop'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('poszukiwania:password_change_done')), name='password_change'),
     path('register/', views.register, name='register'),
     path('search/', views.search, name='search'),
     path('detail/<str:slug>/<int:seconds>/<int:hours>/<int:id>/', views.detail, name='detail'),
