be5fdf1a5ae64e418ce42025917886ba08a4e5d8
Aleooo <aleksander.wiedenski@gmail.com>
2021-11-24 23:41:41 +0100

    coupon model and tests

--- sklep_2/coupon/models.py
+++ sklep_2/coupon/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50)
+    discount = models.DecimalField(decimal_places=2, max_digits=3)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    active = models.BooleanField()
+
+    
+
