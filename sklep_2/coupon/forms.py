86ad183fd3ea37b035b58b51929d9b74b33c7852
Aleooo <aleksander.wiedenski@gmail.com>
2021-11-26 23:18:32 +0100

    adding style to coupon in cart.html

--- sklep_2/coupon/forms.py
+++ sklep_2/coupon/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 
 class CouponForm(forms.Form):
     code = forms.CharField(max_length=50)
+
+    code.widget.attrs.update({'class': 'text_a_c border_1slg', 'id': 'coupon_input', 'placeholder': _('coupon')})
+   
