35825283789217d6e930c50088eed29e94381f87
Aleooo <aleksanderwiedenski1@gmail.com>
2021-12-16 18:55:00 +0100

    alignment of order form and separation number phone and code

--- sklep_2/order/forms.py
+++ sklep_2/order/forms.py
@@ -1,6 +1,8 @@
 import string
 
 from django import forms
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 from .models import Order
 
@@ -14,16 +16,27 @@ class OrderForm(forms.ModelForm):
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'first_name'}),
             'last_name': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'last_name'}),
-            'email': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'email'}),
-            'phone_number': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'phone_number'}),
+            'email': forms.TextInput(attrs={'class': 'address_form form-control margin_t_05', 'placeholder': 'email'}),
+            'phone_number': PhoneNumberPrefixWidget(initial='PL', attrs={'class': 'address_form form-control phonenumber'}),
             'street': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'street'}),
             'street_number': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'street_number'}),
             'ZIP_code': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'ZIP_code'}),
             'town': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'town'}),
             'country': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'country'}),
         }
+        labels = {
+            'first_name': _('First Name'),
+            'last_name': _('Last Name'),
+            'email': _('Email'),
+            'phone_number': _('Phone Number'),
+            'street': _('Street'),
+            'street_number': _('Street Number'),
+            'ZIP_code': _('ZIP code'),
+            'town': _('Town'),
+            'country': _('Country')
+        }
     
-    
+    # 'phone_number': forms.TextInput(attrs={'class': 'address_form form-control', 'placeholder': 'phone_number'}),
     def clean(self):
         first_name = self.cleaned_data['first_name']
         field_upper = {'first_name': 'First Name', 'last_name': 'Last Name', 'street': 'Street', 'town': 'Town', 'country': 'Country'}
--- sklep_2/shop/forms.py
+++ sklep_2/shop/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import fields
+from django.forms import fields, widgets
 from django.utils.translation import gettext_lazy as _
 
 from .models import Address, UserModel
@@ -15,10 +15,11 @@ class UserModelForm(UserCreationForm):
                    'email': forms.TextInput(attrs={'class': 'register_input', 'placeholder': _('email')}),
                    'password1': forms.TextInput(attrs={'class': 'register_input', 'placeholder': _('password')}),
                    'password2': forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': _('password again')}),
-                   'number': forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': _('number')})}
-    
+                     'number': forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': _('number')})}
     error_messages = {
         'password_mismatch': _('The two password fields didn`t match'),
     }
+
+
 
 
