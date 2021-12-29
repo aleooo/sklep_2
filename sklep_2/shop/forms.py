32505df1f44b988c7ccabce99f1b4de01536852c
Aleooo <aleksanderwiedenski1@gmail.com>
2021-12-30 00:25:10 +0100

    form validation

--- sklep_2/shop/forms.py
+++ sklep_2/shop/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import fields, widgets
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 from .models import Address, UserModel
 
@@ -11,15 +12,15 @@ class UserModelForm(UserCreationForm):
     class Meta:
         model = UserModel 
         fields = ('username', 'email', 'password1', 'password2', 'number')
-        widgets = {'username': forms.TextInput(attrs={'class': 'register_input', 'placeholder': _('username')}),
-                   'email': forms.TextInput(attrs={'class': 'register_input', 'placeholder': _('email')}),
-                   'password1': forms.TextInput(attrs={'class': 'register_input', 'placeholder': _('password')}),
-                   'password2': forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': _('password again')}),
-                     'number': forms.PasswordInput(attrs={'class': 'register_input', 'placeholder': _('number')})}
+        widgets = {'username': forms.TextInput(attrs={'class': 'register_input form-control shadow-none', 'placeholder': _('username')}),
+                   'email': forms.TextInput(attrs={'class': 'register_input form-control shadow-none', 'placeholder': _('email'), 'type': 'email'}),
+                   'password1': forms.TextInput(attrs={'class': 'register_input form-control shadow-none', 'placeholder': _('password')}),
+                   'password2': forms.PasswordInput(attrs={'class': 'register_input form-control shadow-none', 'placeholder': _('password again')}),
+                     'number': PhoneNumberPrefixWidget(initial='PL', attrs={'class': 'register_input address_form form-control shadow-none  phonenumber'})}
+                     
     error_messages = {
         'password_mismatch': _('The two password fields didn`t match'),
     }
-
 
 
 
